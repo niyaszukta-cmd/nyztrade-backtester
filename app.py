c1eaf172a44b46086a5a1cc2bf5bd4c2a28b1fae
Dr Niyas N (nyztrade) <niyaszukta@gmail.com>
2026-02-28 00:30:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ from dataclasses import dataclass
 @dataclass
 class DhanConfig:
     client_id:    str = "1100480354"
-    access_token: str = "paste_your_token_here"   # ← paste fresh token here daily
+    access_token: str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzcyMjU3MTI2LCJhcHBfaWQiOiJjOTNkM2UwOSIsImlhdCI6MTc3MjE3MDcyNiwidG9rZW5Db25zdW1lclR5cGUiOiJBUFAiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwMDQ4MDM1NCJ9.kJ6mEUuujMFz8Bte9MCdsg_lF94hTq1xtygSBLzXqhYCb2JfvLbAEYS7pOJtEV424qEgCTg80N4c_a2rRMRRZQ"   # ← paste fresh token here daily
 
 DHAN_BASE = "https://api.dhan.co/v2"
 
